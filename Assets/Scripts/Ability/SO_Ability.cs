309ee10ab778a73ba283b227d78e10c099882c68
VoidAwake <voidawake90@gmail.com>
2022-09-02 14:21:19 +1000

    Remove E_Ability enum

--- Assets/Scripts/Ability/SO_Ability.cs
+++ Assets/Scripts/Ability/SO_Ability.cs
@@ -1,25 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public enum E_Ability //enter abilities as we go here
-{
-    DebugNorth,
-    DebugWest,
-    DebugSouth,
-    Dodge,
-    Sweep,
-    Jab,
-    Bomb,
-    Boomerang,
-}
-
-
 
 [CreateAssetMenu(fileName = "Untitled", menuName = "ScriptableObjects/Ability", order = 1)]
 public class SO_Ability : ScriptableObject
 {
-    public E_Ability abilityType;
     public SO_Element element;
 
     public float damage;
