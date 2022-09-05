c6ecfeddb5966ce79378b1e9b9824ed25c4ab1f8
BillingtonBigCat <wmajor21@gmail.com>
2022-09-05 11:38:09 +1000

    BeamWorkingWithOtherSystems

--- Assets/Scripts/Ability/BeamAbility.cs
+++ Assets/Scripts/Ability/BeamAbility.cs
@@ -5,22 +5,28 @@ using Myths;
 
 public class BeamAbility : Ability
 {
+    private float BeamTimer;
+    private float BeamDuration;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BeamDuration = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BeamTimer += Time.deltaTime;
+        if (BeamTimer > BeamDuration)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public override void Trigger(Myth myth)
     {
         Attack(myth, ability.damage); //Called In The Parent Ability
-        Debug.LogWarning($"Swipe Collided With Object: {myth.gameObject.name}");
+        Debug.LogWarning($"Beam Collided With Object: {myth.gameObject.name}");
         base.Trigger(myth);
     }
 }
