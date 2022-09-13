dc710a812bf36058c496f845f686069c274e3d4b
BillingtonBigCat <wmajor21@gmail.com>
2022-09-13 17:19:41 +1000

    Adjusted Beam

--- Assets/Scripts/Ability/BeamAbility.cs
+++ Assets/Scripts/Ability/BeamAbility.cs
@@ -5,24 +5,54 @@ using Myths;
 
 public class BeamAbility : Ability
 {
-    private float BeamTimer;
-    private float BeamDuration;
+    private float ChargeTimer;
+    private float DurationTimer;
+
+    [SerializeField] private float BeamDuration;
+    [SerializeField] private float BeamChargeTime;
+    [SerializeField] private float BeamLength;
+
+    BeamExtender BeamExtender;
+    BeamHead BeamHead;
+
+    private Transform[] beamChildren;
+
     // Start is called before the first frame update
     public override void Start()
     {
         base.Start();
-        BeamDuration = 3.0f;
+        BeamExtender = gameObject.GetComponentInChildren<BeamExtender>();
+        BeamHead = gameObject.GetComponentInChildren<BeamHead>();
+
+        BeamExtender.SetMaxRange(BeamLength);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        BeamTimer += Time.deltaTime;
-        if (BeamTimer > BeamDuration)
+        ChargeTimer += Time.deltaTime;
+        DurationTimer += Time.deltaTime;
+
+        if (ChargeTimer > BeamChargeTime)
+        {
+            if (BeamHead)
+            {
+                BeamHead.Activate();
+            }
+
+            if (BeamExtender)
+            {
+                BeamExtender.Activate();
+            }
+        }
+
+        if (DurationTimer > BeamDuration)
         {
             Destroy(this.gameObject);
         }
     }
+
 
     public override void Trigger(Myth myth)
     {
