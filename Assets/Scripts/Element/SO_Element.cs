49d9923b4600b20b7fbddf13f658d65534d6a5bb
VoidAwake <voidawake90@gmail.com>
2022-09-16 23:46:11 +1000

    Add element strengths to scriptable objects

--- Assets/Scripts/Element/SO_Element.cs
+++ Assets/Scripts/Element/SO_Element.cs
@@ -1,14 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Untitled", menuName = "ScriptableObjects/Element", order = 1)]
 public class SO_Element : ScriptableObject
 {
-    //an array for effective-against and ineffective-against??
-
     // TODO: Not ideal to be storing this here, elements shouldn't know about debris
     public GameObject debrisBehavioursPrefab;
 
     public Color color;
     
     // TODO: Move element icon here too
+
+    public List<SO_Element> strongAgainst = new();
 }
