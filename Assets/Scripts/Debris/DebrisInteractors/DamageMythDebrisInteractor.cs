b7377f55ca31df4ebb3a7d7b3604d66ae89d0b8d
VoidAwake <voidawake90@gmail.com>
2022-09-21 17:40:12 +1000

    Add element filter component

--- Assets/Scripts/Debris/DebrisInteractors/DamageMythDebrisInteractor.cs
+++ Assets/Scripts/Debris/DebrisInteractors/DamageMythDebrisInteractor.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Myths;
 using UnityEngine;
 
@@ -8,14 +7,12 @@ namespace Debris.DebrisInteractors
     {
         [SerializeField] private Myth myth;
         [SerializeField] private float damagePerSecond;
-        [Tooltip("The value returned by the filter when the filter list contains the element.")]
-        [SerializeField] private bool filterType;
-        [SerializeField] private List<SO_Element> filterElements;
+        [SerializeField] private ElementFilter elementFilter;
 
         // TODO: Add a check for if the myth is of the same element as the debris
         internal override void OnDebrisStay(Debris debris)
         {
-            if (filterType == filterElements.Contains(debris.CurrentElement)) 
+            if (elementFilter.PassesFilter(debris.CurrentElement)) 
                 myth.TakeDamage(damagePerSecond * Time.deltaTime);
         }
     }
