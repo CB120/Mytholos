d2086a27cb9be9f1a62208f463e2dce726858651
CB120 <burgio120@gmail.com>
2022-09-25 02:24:03 +1000

    [GDS2-Dodge Improvements] Fixed up some of the velocity issues with dodge, synced animations up with it also

--- Assets/Scripts/Commands/Behaviours/IdleBehaviour.cs
+++ Assets/Scripts/Commands/Behaviours/IdleBehaviour.cs
@@ -1,7 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
 namespace Myths.Behaviours
 {
     public class IdleBehaviour : Behaviour
     {
-        
+        public NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = myth.GetComponent<NavMeshAgent>();
+            if (navMeshAgent == null)
+            {
+                Debug.Log("There was a problem assigning " + myth.gameObject.name + " to the navmesh");
+            }
+        }
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            navMeshAgent.ResetPath();
+            Debug.Log("Idling");
+        }
     }
 }
