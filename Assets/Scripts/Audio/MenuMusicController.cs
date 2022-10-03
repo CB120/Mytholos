657b9ff78dca8321caf31942fabd16e60e2e7b14
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2022-10-03 22:32:17 +1100

    (GDS2-124) Menu Music Reworked per Eddie's Request

--- Assets/Scripts/Audio/MenuMusicController.cs
+++ Assets/Scripts/Audio/MenuMusicController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using FMODUnity;
+using UnityEngine.InputSystem.LowLevel;
 
 public enum E_MenuState
 {
@@ -10,14 +11,31 @@ public enum E_MenuState
     ArenaSelect = 4
 }
 
+[System.Serializable]
+public class MenuMusicTrack : MusicLayer
+{
+    [Tooltip("Menu State")]
+    public E_MenuState state;
+}
+
 public class MenuMusicController : MonoBehaviour
 {
     // Properties
+    [Header("Properties")]
     [Tooltip("If True, enables Don't Destroy On Load for this GameObject")]
     public bool persistent = true;
+    [Tooltip("% volume per second | Larger values = faster crossfades")]
+    public float fadeInRate = 40f;
+    [Tooltip("% volume per second | Larger values = faster crossfades")]
+    public float fadeOutRate = 20f;
 
+    [Space(30)]
+    [Tooltip("All Menu Music Tracks")]
+    public MenuMusicTrack[] musicTracks;
 
     // Variables
+    [Space(30)]
+    [Header("Debug Readouts")]
     [SerializeField] private E_MenuState currentMenuState = E_MenuState.MainMenu;
 
 
@@ -36,13 +54,6 @@ public class MenuMusicController : MonoBehaviour
             return;
         }
 
-        // Hm
-        //foreach (MenuMusicController musicController in FindObjectsOfType<MenuMusicController>())
-        //{
-        //    if (musicController != this)
-        //        Destroy(gameObject);
-        //}
-
         Singleton = this;
         if (persistent) DontDestroyOnLoad(gameObject);
 
@@ -54,15 +65,73 @@ public class MenuMusicController : MonoBehaviour
         ChangeMenuState(currentMenuState); //allows you to change the default state in the Inspector
     }
 
+    private void Update()
+    {
+        UpdateFades();
+        UpdateTrackVolumes();
+    }
+
 
     // Methods
         // Public
+            // Static
     public static void ChangeMenuState(E_MenuState newState)
     {
         if (Singleton != null)
         {
-            Singleton.currentMenuState = newState;
-            Singleton.menuMusicEmitter.SetParameter("Menu Music", (int)Singleton.currentMenuState);
+            Singleton.SetMenuState(newState);
+        } else
+        {
+            Debug.LogWarning("Singleton == null | Couldn't find a MenuMusicController!");
         }
+    }
+
+            // Non-Static
+    public void SetMenuState(E_MenuState state)
+    {
+        GetTrackWithState(currentMenuState).targetVolume = 0f;
+        GetTrackWithState(state).targetVolume = 100f;
+        currentMenuState = state;
+    }
+
+
+        // Private
+    void UpdateFades() //applies the lerp between the current volume and target volume
+    {
+        foreach (MenuMusicTrack m in musicTracks)
+        {
+            if (m.manualVolumeOverride) continue; //GUARD to allow manualVolumeOverride to disable the automated fading
+
+            //Adding/subtracting a constant per second = linear transition.
+            //Adjust the volume curve in FMOD, don't try and change the lerp curve
+            if (m.volume > m.targetVolume) m.volume -= fadeOutRate * Time.deltaTime;
+            if (m.volume < m.targetVolume) m.volume += fadeInRate * Time.deltaTime;
+            if (Mathf.Abs(m.volume - m.targetVolume) <= 0.1f) m.volume = m.targetVolume;
+
+            //If we end up with targetVolumes that aren't 0 or 100, add an 'if volume delta <= 0.1, volume = target' here
+
+            m.volume = Mathf.Clamp(m.volume, 0f, 100f);
+        }
+    }
+
+    void UpdateTrackVolumes() //applies the current volumes to FMOD
+    {
+        foreach (MenuMusicTrack m in musicTracks)
+        {
+            menuMusicEmitter.SetParameter(m.name + " Volume", m.volume); //Syntax in FMOD is currently '<Layer> Volume', e.g 'Main Menu Volume'
+        }
+    }
+
+
+    // Functions
+    MenuMusicTrack GetTrackWithState(E_MenuState state)
+    {
+        foreach (MenuMusicTrack m in musicTracks)
+        {
+            if (m.state == state) return m;
+        }
+
+        Debug.LogWarning("Could not find a Menu Music Track with E_MenuState " + state + ". Returning null, errors expected.");
+        return null;
     }
 }
--- Assets/Scripts/Audio/MenuMusicResetter.cs
+++ Assets/Scripts/Audio/MenuMusicResetter.cs
@@ -6,7 +6,9 @@ public class MenuMusicResetter : MonoBehaviour
 {
     void Start()
     {
-        MenuMusicController.ChangeMenuState(E_MenuState.MainMenu);
+        //In every other Menu Scene the relevant controller updates the MenuMusicController's state. It defaults to MainMenu, but if the Player(s)
+        //return to the Main Menu, there's no controller to reset it, so this does!
+        MenuMusicController.ChangeMenuState(E_MenuState.MainMenu); 
         Destroy(gameObject);
     }
 }
