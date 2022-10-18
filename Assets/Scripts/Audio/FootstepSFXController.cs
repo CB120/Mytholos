2efa55b5258ea594a015cdde7df49f1175102e29
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2022-10-18 15:25:39 +1100

    (GDS2-168) Footsteps Implemented
    Dear lord that was way more effort than it needed to be

--- Assets/Scripts/Audio/FootstepSFXController.cs
+++ Assets/Scripts/Audio/FootstepSFXController.cs
@@ -7,25 +7,62 @@ public class FootstepSFXController : MonoBehaviour
     // Properties
     [Header("Properties")]
     public float timeToDestroySFX = 0.2f;
+    public bool playFrontStepOnLeft = true;
 
 
     // Variables
 
 
     // References
+    [Header("In-Prefab References")]
+    public Transform frontLeftLeg;
+    public Transform frontRightLeg;
+    public Transform backLeftLeg;
+    public Transform backRightLeg;
+
     [Header("SFX Prefabs")]
     public GameObject frontStepSFXPrefab;
     public GameObject backStepSFXPrefab;
 
 
     // Animation-Event Called
-    public void PlayFrontStep(Transform targetTransform)
+    public void FrontStep()
+    {
+        Transform target = frontRightLeg;
+        if (playFrontStepOnLeft) target = frontLeftLeg;
+        PlayFrontStep(target);
+    }
+
+    public void FrontLeftStep()
+    {
+        PlayBackStep(frontLeftLeg); // yes, I know it says PlayBackStep(), this is because the function called determines the sound played, and FrontStep is louder, only for the Run animation
+    }
+
+    public void FrontRightStep()
+    {
+        PlayBackStep(frontRightLeg); // so basically yes, this isn't a mistake :)
+    }
+
+    public void BackLeftStep()
+    {
+        PlayBackStep(backLeftLeg);
+    }
+
+    public void BackRightStep()
+    {
+        PlayBackStep(backRightLeg);
+    }
+
+
+    // Methods
+        // Private
+    void PlayFrontStep(Transform targetTransform)
     {
         GameObject sfx = Instantiate(frontStepSFXPrefab, targetTransform);
         Destroy(sfx, timeToDestroySFX);
     }
 
-    public void PlayBackStep(Transform targetTransform)
+    void PlayBackStep(Transform targetTransform)
     {
         GameObject sfx = Instantiate(backStepSFXPrefab, targetTransform);
         Destroy(sfx, timeToDestroySFX);
