ccbbfa60b08aaa2cd7a494a7844fdc716c4b6fc6
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2022-10-21 19:19:21 +1100

    (-) Mouse Cursor Hidden

---
+++ Assets/Scripts/Utilities/MouseHiderUtility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHiderUtility : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+}
