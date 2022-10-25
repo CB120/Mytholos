4f5fc048232f8c7f69c13a7a44b83f53ee397da6
VoidAwake <voidawake90@gmail.com>
2022-10-25 16:15:44 +1100

    Separate ResultsUI from WinState

--- Assets/TemporaryWinScreen/WinState.cs
+++ Assets/TemporaryWinScreen/WinState.cs
@@ -1,25 +1,15 @@
-using System.Collections;
+using System;
 using Myths;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class WinState : MonoBehaviour
 {
     private int team1Remaining = 3;
     private int team2Remaining = 3;
-    //public GameObject obj;
-    //public TextMeshProUGUI text;
-
-    [Header("Scene References")]
-    [SerializeField] CanvasGroup gameplayUI;
-    [SerializeField] CanvasGroup resultsUI;
-    [SerializeField] Image playerWinImage;
-    [SerializeField] UIMenuNodeGraph resultsMenu;
-
-    [Header("Asset References")]
-    [SerializeField] Sprite[] playerWinSprites;
     [SerializeField] private MythRuntimeSet mythRuntimeSet;
+
+    [NonSerialized] public UnityEvent<int> gameEnded = new();
 
     // TODO: Duplicate code. See EpicEddieCam.
     private void OnEnable()
@@ -55,18 +45,12 @@ public class WinState : MonoBehaviour
 
     private void DecreaseScore(int partyIndex)
     {
-        //print("Decrease score! Player " + partyIndex);
-        //print("Team 1: " + team1Remaining + ", Team 2: " + team2Remaining + "(before)");
-
         if (partyIndex == 0)
         {
             team1Remaining--;
             if (team1Remaining == 0)
             {
-                EndGame((partyIndex + 1) % 2);
-                //obj.SetActive(true);
-                //StartCoroutine(AnyButtonCoroutine());
-                //text.text = "Team 1 was Defeated";
+                gameEnded.Invoke(1);
             }
         }
         else if (partyIndex == 1)
@@ -74,114 +58,8 @@ public class WinState : MonoBehaviour
             team2Remaining--;
             if (team2Remaining == 0)
             {
-                EndGame((partyIndex + 1) % 2);
-                //obj.SetActive(true);
-                //StartCoroutine(AnyButtonCoroutine());
-                //text.text = "Team 2 was Defeated";
+                gameEnded.Invoke(0);
             }
         }
-
-        //print("Team 1: " + team1Remaining + ", Team 2: " + team2Remaining + "(after)");
     }
-
-    void EndGame(int winningPlayerIndex)
-    {
-        //print("Well, we tried");
-
-        resultsUI.gameObject.SetActive(true);
-
-        // Swap player input controls schemes, disable their inputs momentarily for duration of transition
-        foreach (PlayerParticipant participant in FindObjectsOfType<PlayerParticipant>())
-        {
-            // Update the winning player's current UI graph to the results menu
-            //if (participant.partyIndex == winningPlayerIndex)
-            //    participant.currentMenuGraph = resultsMenu;
-
-            //participant.DisablePlayerInput(0.35f);
-            //PlayerInput input = participant.GetComponent<PlayerInput>();      // Temporarily commented out
-            //input.actions.FindActionMap("Player").Disable();
-            //input.actions.FindActionMap("UI").Enable();
-
-            participant.DisablePlayerInput(2.0f);
-        }
-
-        StartCoroutine(UhhReloadTheScene());
-
-        // Begin transition to fade out gameplay UI and fade in results UI
-        StartCoroutine(FadeGameOutFadeResultsIn(0.35f));
-
-        // Do something with the camera
-        FindObjectOfType<EpicEddieCam>().FocusOnSingleMyth(winningPlayerIndex);
-
-        // Set winning text image based on winning player index
-        playerWinImage.sprite = playerWinSprites[winningPlayerIndex];
-    }
-
-    IEnumerator UhhReloadTheScene()
-    {
-        yield return new WaitForSeconds(2.0f);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    IEnumerator FadeGameOutFadeResultsIn(float duration)
-    {
-        float timer = 0;
-        Vector2 originalSize = playerWinImage.rectTransform.sizeDelta;
-        float cycles = Mathf.PI * 5.0f / (duration + 0.5f);
-
-        while (timer < duration)
-        {
-            resultsUI.alpha = timer / duration;
-            gameplayUI.alpha = 1 - timer / duration;
-
-            playerWinImage.rectTransform.sizeDelta = originalSize * Mathf.Lerp((0.8f + 0.4f * Mathf.Sin(cycles * timer)), 1, timer / (duration + 0.5f));
-
-            timer += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-
-        resultsUI.alpha = 1.0f;
-        gameplayUI.alpha = 0.0f;
-
-        while (timer < duration + 0.5f)
-        {
-            playerWinImage.rectTransform.sizeDelta = originalSize * Mathf.Lerp((0.8f + 0.4f * Mathf.Sin(cycles * timer)), 1, timer / (duration + 0.5f));
-
-            timer += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-
-        playerWinImage.rectTransform.sizeDelta = originalSize;
-    }
-
-    //private IEnumerator AnyButtonCoroutine()
-    //{
-    //    yield return new WaitForSeconds(1);
-
-    //    InputSystem.onEvent += OnAnyButtonPressed;
-    //}
-
-    //public void RestartGame()
-    //{
-    //    obj.SetActive(false);
-
-    //    InputSystem.onEvent -= OnAnyButtonPressed;
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    //}
-
-    //// TODO: Copied from Round Robbin. Not optimal at all.
-    //private void OnAnyButtonPressed(InputEventPtr eventPtr, InputDevice device)
-    //{
-    //    if (!eventPtr.IsA<StateEvent>() && !eventPtr.IsA<DeltaStateEvent>()) return;
-
-    //    // Copied from InputUser, works somehow.
-    //    foreach (var control in eventPtr.EnumerateChangedControls(device: device, magnitudeThreshold: 0.0001f))
-    //    {
-    //        if (control == null || control.synthetic || control.noisy) continue;
-
-    //        RestartGame();
-
-    //        break;
-    //    }
-    //}
 }
